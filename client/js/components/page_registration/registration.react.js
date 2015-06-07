2fa1455ca67daf118438d97ddcc86bf15aa0a537
Emilia <e.andrzejewska@pilot.co>
2015-06-08 00:26:07 +0200

    Registration process

--- client/js/components/page_registration/registration.react.js
+++ client/js/components/page_registration/registration.react.js
@@ -2,62 +2,59 @@
 
 var React = require('react');
 var Header = require('../common/header.react.js');
+var RegistrationForm = require('./form.react.js');
+var RegistrationLoading = require('./loading.react.js');
+var RegistrationSuccess = require('./success.react.js');
+var RegistrationError = require('./error.react.js');
 
 var RegistrationPage = React.createClass({
+  getInitialState: function(){
+    return {currentView: "form"}
+  },
+
+  createRegistration: function(action, data){
+    this.setState({currentView: "loading"});
+
+     $.ajax({
+        type: 'POST',
+        url: action,
+        data: data,
+        success: function() {
+          this.setState({currentView: "success"})
+        }.bind(this),
+        error: function(xhr, status, err) {
+          this.setState({currentView: "error"})
+        }.bind(this)
+      });
+  },
+
   render: function() {
-    var pageData = JSON.parse(localStorage.getItem('pages'))[1];
+    var pageData = JSON.parse(localStorage.getItem('pages'))[1],
+        content = null;
+
+    switch(this.state.currentView) {
+      case "form":
+          content = <RegistrationForm onHandleSubmit={this.createRegistration}/>
+          break;
+      case "loading":
+          content = <RegistrationLoading/>
+          break;
+      case "success":
+          content = <RegistrationSuccess/>
+          break;
+      case "error":
+          content = <RegistrationError/>
+          break;
+      default:
+          content = <RegistrationForm onHandleSubmit={this.createRegistration}/>
+    }
 
     return (
       <div>
         <Header {...pageData}/>
-         <form name='registration' action='/registration' method='post'>
-            <div className='container container--registration'>
-              <div className='row registration__section'>
-                <div className='col-sm-12 registration__subtitle'>Dane uczestnika warsztatów</div>
-                <div className='col-sm-6'>
-                  <label className='registration__label' for='child.firstname'>
-                    Imię <span className='registration__info'>(pole wymagane)</span>
-                  </label> 
-                  <input className='registration__input' id='child.firstname' name='child.firstname' type='text' placeholder='Imię dziecka' required></input>
-                </div>
-
-                <div className='col-sm-6'>
-                  <label className='registration__label' for='child.lastname'>
-                    Nazwisko <span className='registration__info'>(pole wymagane)</span>
-                  </label>
-                  <input className='registration__input' id='child.lastname' name='child.lastname' type='text' placeholder='Nazwisko dziecka' required></input>
-                </div>
-              </div>
-              <div className='row registration__section'>
-                <div className='col-sm-12 registration__subtitle'>Dane rodzica/opiekuna dziekca</div>
-                <div className='col-sm-6'>
-                  <label className='registration__label' for='firstname'>
-                    Imię <span className='registration__info'>(pole wymagane)</span>
-                  </label>
-                  <input className='registration__input' id='firstname' name='firstname' type='text' placeholder='Imię rodzica/opiekuna' required></input>
-                </div>
-
-                <div className='col-sm-6'>
-                  <label className='registration__label' for='lastname'>
-                    Nazwisko <span className='registration__info'>(pole wymagane)</span>
-                  </label>
-                  <input className='registration__input' id='lastname' name='lastname' type='text' placeholder='Nazwisko rodzica/opiekuna' required></input>
-                </div>
-
-                <div className='col-sm-6'>
-                  <label className='registration__label' for='email'>
-                    Adres E-mail <span className='registration__info'>(pole wymagane)</span>
-                  </label>
-                  <input className='registration__input' id='email' name='email' type='email' required></input>
-                </div>
-              </div>
-              <div className='row registration__section'>
-                <div className='col-sm-12'>
-                  <button id='accept' className='btn-hck'>Wyślij zgłoszenie</button>
-                </div>
-              </div>
-            </div>
-          </form>
+          <div className='container container--registration'>
+            {content}
+         </div>
       </div>
     );
   }
---
+++ client/js/components/page_registration/success.react.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var React = require('react');
+
+var RegistrationSuccess = React.createClass({
+  render: function() {
+    return (
+      <div className='info--centered'>
+        <img className='info__robot-image' src="./images/logo_robot_hackidemia.png"></img>
+        <br></br>
+        <span>Rejestracja została zapisana w systemie.</span>
+        <br></br>
+        <span>Potwierdzenia uczestnictwa można dokonać za pomocą linka przesłanego na podany adres e-mail.</span>
+      </div>
+    );
+  }
+});
+
+module.exports = RegistrationSuccess;
