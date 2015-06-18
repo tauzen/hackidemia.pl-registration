6f35bf06f19c9be0347b8936b2be7fb58689557f
Krzysztof Mioduszewski <kmioduszewski@gmail.com>
2015-06-18 13:08:04 +0200

    server side validation

--- server/src/registration_model.js
+++ server/src/registration_model.js
@@ -1,26 +1,50 @@
 'use strict';
 
 var mongoose = require('mongoose');
+var validate = require('mongoose-validator');
+var uniqueValidator = require('mongoose-unique-validator');
+
+var nameValidator = [
+  validate({
+    validator: 'isLength',
+    arguments: [3, 50],
+    message: '{PATH} should be between {ARGS[0]} and {ARGS[1]} characters'
+  }),
+  validate({
+    validator: 'isAlphanumeric',
+    message: 'Name should contain aplha-numeric chracters only'
+  })
+];
+
+var emailValidator = validate({
+  validator: 'isEmail',
+  message: 'Valid email required'
+});
  
 var registrationSchema = new mongoose.Schema({
   ObjectId: mongoose.Schema.ObjectId,
   date: { type: Date, default: Date.now },
   child: {
-    firstname: { type: String, required: true },
-    lastname: { type: String, required: true },
+    firstname: { type: String, required: true, validate: nameValidator },
+    lastname: { type: String, required: true, validate: nameValidator },
     age: { type: Number, min: 1, max: 18 },
-    info: { type: String }
+    info: { type: String, maxlength: 255 }
   },
   email: { 
     type: String,
     unique: true,
-    required: true
+    required: true,
+    validate: emailValidator
   },
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
+  firstname: { type: String, required: true, validate: nameValidator },
+  lastname: { type: String, required: true, validate: nameValidator },
   phone: { type: String },
   confirmed: { type: Boolean, default: false },
   token: { type: String }
 });
+
+// using message to distinguish unique violation from
+// other validation errors
+registrationSchema.plugin(uniqueValidator, { message: 'unique' });
  
 module.exports = mongoose.model('Registration', registrationSchema);
