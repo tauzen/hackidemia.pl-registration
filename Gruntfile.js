543274d3e6328b529532e1d02973273c91402c11
Krzysztof Mioduszewski <kmioduszewski@gmail.com>
2015-06-24 22:13:18 +0200

    nit, grunt watching js files in subdirectories

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function(grunt) {
         }
       },
       browserify: {
-        files: 'client/js/*.js',
+        files: 'client/js/**/*.js',
         tasks: ['browserify:dev']
       }
     }
