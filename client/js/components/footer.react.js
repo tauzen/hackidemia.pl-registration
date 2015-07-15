8cd67b3411d531726a4d921d543586a4496d5b7f
Krzysztof Mioduszewski <kmioduszewski@gmail.com>
2015-07-15 18:21:38 +0200

    added partner link and logo

--- client/js/components/footer.react.js
+++ client/js/components/footer.react.js
@@ -5,10 +5,13 @@ var React = require('react');
 var Footer = React.createClass({
   render: function() {
     return (
-      <div className='footer'> 
+      <div className='footer'>
+        <div className='footer__partners'>
+          <p>Partnerzy medialni</p>
+          <a className='footer__crossweb' href="http://crossweb.pl/" target='_blank'><img src='/static/images/crossweb-logo.png' /></a>
+        </div>
         <div className='footer__inner'>
           Hackidemia is licensed under a <a href="http://creativecommons.org/licenses/by-nc/3.0/deed.en_US">Creative Commons Attribution-NonCommercial 3.0 Unported License</a>
-            
             <div className='footer__contact'>
               <div className='circle'>
                 <a href="https://twitter.com/HacKidemia" target="_blank" className="twitter">
